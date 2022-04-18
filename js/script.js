ca05eecb0bfaeaab22c414cb122297a43f004aa9
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-18 14:14:11 +0000

    js fix

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 // This file contains the JS functions for index.html
 
 'use strict'
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
 /**
  * This function calculates volume of a sphere.
  */
